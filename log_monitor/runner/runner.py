2f037f85f02703a645bca00a83b4d18b2c1249cc
Eloi Charpentier <eloi.charpentier@prologin.org>
2021-02-09 14:30:45 +0100

    runner: add runner class, with default config

---
+++ log_monitor/runner/runner.py
@@ -0,0 +1,27 @@
+import time
+from ..models import LogEntry
+
+
+class Runner:
+    """
+    This class manages running all the tasks and watching for file updates
+    """
+    def __init__(self, config_dict):
+        # TODO fill tasks
+        self.tasks = []
+        self.watched_file = open(config_dict["log_file"], "r")
+        self.watched_file.seek(0, 2)    # skip to the end of file
+
+    def _register_entry(self, log_entry):
+        for task in self.tasks:
+            task.register_entry(log_entry)
+
+    def run(self):
+        while True:
+            new_text = self.watched_file.read()
+            if new_text:
+                for entry_txt in new_text.split("\n"):
+                    entry = LogEntry(entry_txt)
+                    self._register_entry(entry)
+            else:
+                time.sleep(0.1)
