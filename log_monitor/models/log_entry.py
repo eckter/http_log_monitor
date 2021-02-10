b1781fb9f05cf7314a2168ccd939038cf44667cd
Eloi Charpentier <eloi.charpentier@prologin.org>
2021-02-10 14:04:16 +0100

    models: log_entry: improve CLF parsing using a library

--- log_monitor/models/log_entry.py
+++ log_monitor/models/log_entry.py
@@ -1,17 +1,34 @@
+from clfparser import CLFParser
+
+
 class LogEntry:
     """
     Represents the content of an entry following Common Log Format
     """
     def __init__(self, string_entry):
         try:
-            self.host, \
-                self.identity, \
-                self.user_id, \
-                self.time, \
-                self.request, \
-                self.status, \
-                self.size = string_entry.split()
-
-            self.request = self.request[1:-1]   # removes quotes
-        except ValueError:
+            entry_dict = CLFParser.logDict(string_entry)
+        except ValueError as e:
             raise RuntimeError("Invalid log entry")
+
+        if not self._is_valid(entry_dict):
+            raise RuntimeError("Invalid log entry")
+
+        self.host = entry_dict["h"]
+        self.identity = entry_dict["l"]
+        self.user_id = entry_dict["u"]
+        self.time_str = entry_dict["t"]
+        self.time = entry_dict["time"]
+        self.timezone = entry_dict["timezone"]
+        self.request = entry_dict["r"]
+        self.status = entry_dict["s"]
+        self.size = entry_dict["b"]
+        self.referer = entry_dict["Referer"]
+        self.user_agent = entry_dict["Useragent"]
+
+    @staticmethod
+    def _is_valid(entry_dict):
+        for v in entry_dict.values():
+            if v:
+                return True
+        return False
